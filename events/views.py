4d2cb2e1cfb9ebe0d56100b4de2f2e61c00c16c2
arash <arashsafdarpour@gmail.com>
2025-04-28 12:41:41 +0330

    clean set max_val for addcart

--- events/views.py
+++ events/views.py
@@ -24,7 +24,6 @@ class EventDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         event = context['event']
-        print(context['event'].capacity)
         context['form'] = AddCartForm(capacity=event.capacity)
         return context
 
