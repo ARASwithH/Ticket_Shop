e1ba3978cd7b6b75897051334a23da85453e6f45
arash <arashsafdarpour@gmail.com>
2025-05-18 11:59:57 +0330

    make category slug autofill

--- events/models.py
+++ events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 from accounts.models import User
 
 
@@ -7,7 +8,12 @@ from accounts.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
